be402dad2db0276df69dbed7a0496be5e3f32022
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2024-12-08 14:13:43 +0700

    add about section (raw data)

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Banner from "../components/main/Banner";
 import SkillsSection from "../components/main/SkillsSection";
 import ProjectsSection from "../components/main/ProjectsSection";
+import AboutSection from "../components/main/AboutSection";
 
 const MainPage: React.FC = () => {
   return (
     <div className="mt-10">
       <Banner />
+      <AboutSection />
       <SkillsSection />
       <ProjectsSection />
     </div>
