f177c7cd85520960fa72ba636fdf9f3409fc150b
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2024-12-11 13:50:02 +0700

    add dtos for project

---
+++ BE_Portfolio/DTOs/ProjectsWithTag.cs
@@ -0,0 +1,13 @@
+﻿namespace BE_Portfolio.DTOs
+{
+    public class ProjectsWithTag
+    {
+        public int Id;
+        public string Title;
+        public string Platform;
+        public string Position;
+        public int NumOfMember;
+        public string? Description;
+        public string ImageUrl;
+    }
+}
