f46d0f243ce3964a3ac8c2aeae0acde26a442ba4
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2024-12-11 14:04:13 +0700

    add project controller

---
+++ BE_Portfolio/Controllers/ProjectController.cs
@@ -0,0 +1,28 @@
+﻿using BE_Portfolio.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE_Portfolio.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectController : Controller
+    {
+        private readonly IProject _projectService;
+
+        public ProjectController(IProject projectService)
+        {
+            _projectService = projectService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProjectsWithTags()
+        {
+            var result = await _projectService.GetProjects();
+            if (result == null)
+            {
+                return BadRequest("List empty!");
+            }
+            return Ok(result);
+        }
+    }
+}
