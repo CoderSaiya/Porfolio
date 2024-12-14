0c3a383b43c8f79b9bf28492607fec3be374fe1c
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2024-12-14 13:43:47 +0700

    delete cache

--- BE_Portfolio/Services/Interfaces/IProject.cs
+++
@@ -1,9 +0,0 @@
-﻿using BE_Portfolio.DTOs;
-
-namespace BE_Portfolio.Services.Interfaces
-{
-    public interface IProject
-    {
-        public Task<List<ProjectsWithTag>?> GetProjects();
-    }
-}
