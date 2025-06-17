2312262002f502aaa014109ea39e6570a23afac7
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-06-17 20:53:04 +0700

    scaffold MongoDB context using Mongoose

---
+++ BE/BE_Portfolio/Data/MongoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BE_Portfolio.Models.Commons;
+using BE_Portfolio.Models.Documents;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+public class MongoDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbContext(IOptions<MongoDbSettings> settings)
+    {
+        var client = new MongoClient(settings.Value.ConnectionString);
+        _database = client.GetDatabase(settings.Value.DatabaseName);
+    }
+
+    public IMongoCollection<Project> Projects =>
+        _database.GetCollection<Project>("Projects");
+}
