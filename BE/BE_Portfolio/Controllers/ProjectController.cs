80421c3ea1ad35873c5d23e4b6d537a90b0cd492
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-06-18 10:19:55 +0700

    add controller

--- BE/BE_Portfolio/Controllers/ProjectController.cs
+++ BE/BE_Portfolio/Controllers/ProjectController.cs
@@ -1,28 +1,36 @@
-﻿using BE_Portfolio.Services.Interfaces;
+﻿using BE_Portfolio.DTOs;
+using BE_Portfolio.Models.Documents;
+using BE_Portfolio.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BE_Portfolio.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProjectController : Controller
-    {
-        private readonly IProject _projectService;
+    public class ProjectController(IProjectService projectService) : Controller
+    {   
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> Get() =>
+            Ok(await projectService.GetAllAsync());
 
-        public ProjectController(IProject projectService)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Project>> Get(string id) =>
+            Ok(await projectService.GetByIdAsync(id));
+
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        [RequestSizeLimit(10_000_000)]
+        public async Task<ActionResult<Project>> Create([FromQuery] CreateProjectDto dto)
         {
-            _projectService = projectService;
+            var project = await projectService.CreateAsync(dto);
+            return CreatedAtAction(nameof(Get), new { id = project.Id }, project);
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetProjectsWithTags()
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
         {
-            var result = await _projectService.GetProjects();
-            if (result == null)
-            {
-                return BadRequest("List empty!");
-            }
-            return Ok(result);
+            await projectService.DeleteAsync(id);
+            return NoContent();
         }
     }
 }
